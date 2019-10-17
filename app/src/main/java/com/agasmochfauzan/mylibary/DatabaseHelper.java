6a3885036a2247e0ac99d6f823b768fbdf3be286
agasmoch <agasm5@gmail.com>
2019-10-17 17:10:57 +0700

    Masukin dan manggil database di AS

--- app/src/main/java/com/agasmochfauzan/mylibary/DatabaseHelper.java
+++ app/src/main/java/com/agasmochfauzan/mylibary/DatabaseHelper.java
@@ -1,30 +1,66 @@
 package com.agasmochfauzan.mylibary;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static String DATABASE_NAME = "student_database";
     public static final int DATABASE_VERSION=1;
     public static final String TABLE_STUDENT = "students";
-    public static final String KET_ID="id";
+    public static final String KEY_ID="id";
     public static final String KEY_FIRSTNAME= "name";
 
-    private static final String CREATE_TABLE_STUDENT="";
+    private static final String CREATE_TABLE_STUDENT=""
+            +"CREATE TABLE "+TABLE_STUDENT+"("
+            +KEY_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+KEY_FIRSTNAME+" TEXT);";
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d("table",CREATE_TABLE_STUDENT);
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_STUDENT);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS '"+TABLE_STUDENT+"'");
+        onCreate(sqLiteDatabase);
 
     }
+
+    public long addStudentDetail(String student)//Memasukan nama student
+        {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_FIRSTNAME,student);
+        long insert = db.insert(TABLE_STUDENT, null,values);
+        return insert;
+        }
+
+        public ArrayList<String> getAllStudentList(){
+        ArrayList<String> studentsArrayList = new ArrayList<>();
+        String name= "";
+        String selectQuery = "SELECT * FROM "+TABLE_STUDENT;
+        SQLiteDatabase db = this.getReadableDatabase();
+            Cursor c = db.rawQuery(selectQuery,null);
+            if (c.moveToFirst()){
+                do {
+                    name = c.getString(c.getColumnIndex(KEY_FIRSTNAME));
+                    studentsArrayList.add(name);
+                }while (c.moveToNext());
+            }
+            return studentsArrayList;
+        }
 }
--- app/src/main/java/com/agasmochfauzan/mylibary/MainActivity.java
+++ app/src/main/java/com/agasmochfauzan/mylibary/MainActivity.java
@@ -3,12 +3,55 @@ package com.agasmochfauzan.mylibary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnStore;
+    private Button btnGetAll;
+    private EditText etname;
+    private TextView tvnames;
+    private DatabaseHelper databaseHelper;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStore=findViewById(R.id.btnStore);
+        btnGetAll=findViewById(R.id.btnget);
+        etname=findViewById(R.id.etname);
+        tvnames=findViewById(R.id.tvnames);
+        databaseHelper = new DatabaseHelper(this);
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //tambahkan data
+                databaseHelper.addStudentDetail(etname.getText().toString());
+                etname.setText("");
+                Toast.makeText(MainActivity.this,"Store Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGetAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //tampilkan data
+                arrayList = databaseHelper.getAllStudentList();
+                tvnames.setText("");
+                for (String x:arrayList){
+                    tvnames.setText(tvnames.getText().toString()+", "+x);
+                }
+
+            }
+        });
     }
+
 }
